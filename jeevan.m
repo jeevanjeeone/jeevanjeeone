90c7659f635a9567afa11ae0eb525bb8fa169888
jeevanjeeone <109616243+jeevanjeeone@users.noreply.github.com>
2022-07-19 22:03:40 +0530

    Create jeevan.m

---
+++ jeevan.m
@@ -0,0 +1,11 @@
+- 👋 Hi, I’m @jeevanjeeone
+- 👀 I’m interested in ...coding
+- 🌱 I’m currently learning ...javascript,react js,node js,HTML,CSS
+- 💞️ I’m looking to collaborate on ...front end development jobs
+- 📫 How to reach me ...8951626624
+
+<!---
+jeevanjeeone/jeevanjeeone is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+
